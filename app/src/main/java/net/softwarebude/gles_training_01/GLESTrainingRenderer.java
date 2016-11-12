f98dbce142850536d657b5b52496036c9039c244
Matthias F <matze6633@gmail.com>
2016-11-13 00:33:59 +0100

    implemented basic boilerplate for OpenGL ES 2.0

---
+++ app/src/main/java/net/softwarebude/gles_training_01/GLESTrainingRenderer.java
@@ -0,0 +1,26 @@
+package net.softwarebude.gles_training_01;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import static android.opengl.GLES20.*;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLESTrainingRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
--- app/src/main/java/net/softwarebude/gles_training_01/MainActivity.java
+++ app/src/main/java/net/softwarebude/gles_training_01/MainActivity.java
@@ -1,13 +1,56 @@
 package net.softwarebude.gles_training_01;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        glSurfaceView = new GLSurfaceView(this);
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if(supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            glSurfaceView.setRenderer(new GLESTrainingRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.", Toast.LENGTH_SHORT).show();
+        }
+
+        setContentView(glSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(rendererSet) {
+            glSurfaceView.onResume();
+        }
     }
 }
